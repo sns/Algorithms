878744b909d2911ba180a4ae8784e5a87c56424d
Sina Salehi <sinasalehi@L321-SinaSalehi.local>
2019-11-07 16:33:37 -0500

    fixed lint errors

--- DataStructures/Heap/Heap.ts
+++ DataStructures/Heap/Heap.ts
@@ -1,92 +1,90 @@
 export class Heap {
-    private _isMinHeap: boolean
-    private _data: number[]
-    private _root: Node
+    private _isMinHeap: boolean;
+    private _data: number[];
+    private _root: Node;
 
     constructor(data: number[], isMinHeap: boolean = false) {
-        this.setIsMinHeap(isMinHeap)
-        this.setData(this.buildHeap(data, isMinHeap))
+        this.setIsMinHeap(isMinHeap);
+        this.setData(this.buildHeap(data, isMinHeap));
     }
 
     public getIsMinHeap(): boolean {
-        return this._isMinHeap
+        return this._isMinHeap;
     }
     public setIsMinHeap(isMinHeap: boolean) {
-        this._isMinHeap = isMinHeap
+        this._isMinHeap = isMinHeap;
     }
 
     public getData(): number[] {
-        return this._data
+        return this._data;
     }
 
     public setData(data: number[]) {
-        this._data = data
+        this._data = data;
     }
 
     public getRoot(): Node {
-        return this._root
+        return this._root;
     }
     public setRoot(node: Node) {
-        this._root = node
+        this._root = node;
     }
 
     public maxHeapify(data: number[], index: number, size: number) {
-        let indexOfLargest = index
-        const leftIndex = 2 * index + 1
-        const rightIndex = 2 * index + 2
+        let indexOfLargest = index;
+        const leftIndex = 2 * index + 1;
+        const rightIndex = 2 * index + 2;
         if (leftIndex < size && data[index] < data[leftIndex]) {
-            indexOfLargest = leftIndex
+            indexOfLargest = leftIndex;
         }
         if (rightIndex < size && data[index] < data[rightIndex]) {
-            indexOfLargest = rightIndex
+            indexOfLargest = rightIndex;
         }
 
         if (indexOfLargest !== index) {
-            data = this.swapElements(data, index, indexOfLargest)
-            data = this.maxHeapify(data, indexOfLargest, size)
+            data = this.swapElements(data, index, indexOfLargest);
+            data = this.maxHeapify(data, indexOfLargest, size);
         }
 
-        return data
+        return data;
     }
 
     public minHeapify(data: number[], index: number, size: number) {
-        let indexOfSmallest = index
-        const leftIndex = 2 * index + 1
-        const rightIndex = 2 * index + 2
+        let indexOfSmallest = index;
+        const leftIndex = 2 * index + 1;
+        const rightIndex = 2 * index + 2;
 
         if (leftIndex < size && data[indexOfSmallest] > data[leftIndex]) {
-            indexOfSmallest = leftIndex
+            indexOfSmallest = leftIndex;
         }
         if (rightIndex < size && data[indexOfSmallest] > data[rightIndex]) {
-            indexOfSmallest = rightIndex
+            indexOfSmallest = rightIndex;
         }
 
         if (indexOfSmallest !== index) {
-            data = this.swapElements(data, index, indexOfSmallest)
-            data = this.minHeapify(data, size, indexOfSmallest)
+            data = this.swapElements(data, index, indexOfSmallest);
+            data = this.minHeapify(data, size, indexOfSmallest);
         }
 
-        return data
+        return data;
     }
 
     public buildHeap(data: number[], isMinHeap: boolean = false) {
         for (let index = Math.floor(data.length / 2); index >= 0; index--) {
             data = isMinHeap
                 ? this.minHeapify(data, index, data.length)
-                : this.maxHeapify(data, index, data.length)
+                : this.maxHeapify(data, index, data.length);
         }
 
-        return data
+        return data;
     }
 
     public swapElements(data: any[], index1: number, index2: number) {
-        const swappedData = [...data]
-        ;[swappedData[index1], swappedData[index2]] = [
-            swappedData[index2],
-            swappedData[index1],
-        ]
-        return swappedData
+        const swappedData = [...data];
+        [swappedData[index1], swappedData[index2]] = [swappedData[index2], swappedData[index1]];
+
+        return swappedData;
     }
 }
 
-export default Heap
+export default Heap;
